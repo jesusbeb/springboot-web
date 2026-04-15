8399d7b62eeef0df11d8c7979255678b79848fb2
jesusbeb <jesusbelb@gmail.com>
2026-04-15 13:41:40 -0600

    Clase DTO

--- src/main/java/com/jesusbeb/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/jesusbeb/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -8,15 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import models.User;
+import models.dto.UserDto;
 
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 
+
+    @GetMapping("/details")
+    public UserDto details(){
+
+        User user = new User("JB", "Beltran");
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
+
+        return userDto;
+    }
+
+
+
     // Este metodo retorna un Map que se mostrara como JSON
     // Inicializamos un objeto User para pasarlo al Map
-    @GetMapping("/details")
-    public Map<String, Object> details(){
+    @GetMapping("/details-map")
+    public Map<String, Object> detailsMap(){
         User user = new User("JB", "Beltran");
         //Instanciamos un Map
         Map<String, Object> body = new HashMap<>();
