4ff89897d730fd0aceca4ee86744a97a4c8cde92
jesusbeb <jesusbelb@gmail.com>
2026-04-17 17:24:28 -0600

    Anotacion @ModelAttribute

--- src/main/java/com/jesusbeb/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/jesusbeb/springboot/webapp/springboot_web/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import models.User;
 
@@ -31,14 +32,21 @@ public class UserController {
     // ModelMap de Spring para pasar datos a la Vista, implemente de Map
     @GetMapping("/list")
     public String list(ModelMap model){
-        List<User> users = Arrays.asList(
+            
+        model.addAttribute("title", "Lista de usuarios");
+        return "list";
+    }
+    
+
+    // @ModelAttribute inyecta el valor del atributo "users" a la Vista (plantilla html) antes de ejecutar cualquier handler (metodo) de este controlador
+    // Ya no es necesario agregar el atributo "users" a cada handler (metodo) de este controlador
+    // El valor del atributo "users" se puede usar en cualquier plantilla html de este controlador
+    @ModelAttribute("users")
+    public List<User> usersModel(){
+        return Arrays.asList(
             new User("User4", "lastname4"),
             new User("user5", "lastname5", "user5@email.com"),
             new User("user6", "lastname6", "user6@mail.com"),
             new User("user7", "lastname7"));
-            
-        model.addAttribute("users", users);
-        model.addAttribute("title", "Lista de usuarios");
-        return "list";
     }
 }
