5a6db13e1a5f9772fc6bd06800c29e0e1c212802
jesusbeb <jesusbelb@gmail.com>
2026-04-17 19:24:03 -0600

    Obtener varios parametros de la URL con el objeto request desde HttpServletRequest

--- src/main/java/com/jesusbeb/springboot/webapp/springboot_web/controllers/RequestParamController.java
+++ src/main/java/com/jesusbeb/springboot/webapp/springboot_web/controllers/RequestParamController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import models.dto.ParamDto;
+import models.dto.ParamMixDto;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,12 +25,29 @@ public class RequestParamController {
         return paramDto;
     }
 
+    // Recibiendo dos paramtetros, uno de tipo String y otro de tipo Integer
     @GetMapping("/bar")
-    public ParamDto bar(@RequestParam() String text, @RequestParam Integer code) {
+    public ParamMixDto bar(@RequestParam() String text, @RequestParam Integer code) {
 
-        ParamDto params = new ParamDto();
+        ParamMixDto params = new ParamMixDto();
         params.setMessage(text);
         params.setCode(code);
+        return params;
+    }
+
+    // Otra forma de recibir parámetros, a través del objeto HttpServletRequest
+    // Valida el parámetro "code" para convertirlo a Integer, si no se puede convertir, se asigna el valor 0
+    @GetMapping("/request")
+    public ParamMixDto request(HttpServletRequest request) {
+        Integer code = 0;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) { // Si try no entra, catch no se hace nada y code se queda con el valor 0
+        }
+
+        ParamMixDto params = new ParamMixDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
         return params;
     }
 
--- src/main/java/models/dto/ParamMixDto.java
+++ src/main/java/models/dto/ParamMixDto.java
@@ -1,8 +1,9 @@
 package models.dto;
 
-public class ParamDto {
+public class ParamMixDto {
 
     private String message;
+    private Integer code;
 
     public String getMessage() {
         return message;
@@ -12,5 +13,12 @@ public class ParamDto {
         this.message = message;
     }
 
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
 
 }
