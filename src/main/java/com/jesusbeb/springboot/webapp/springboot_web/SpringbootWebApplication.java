4e54dd15749a47cb1100a44b695b175cd84c24f8
jesusbeb <jesusbelb@gmail.com>
2026-04-22 11:46:14 -0600

    Agregando otros archivos properties personalizados para las configuraciones

--- src/main/java/com/jesusbeb/springboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/jesusbeb/springboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -2,8 +2,14 @@ package com.jesusbeb.springboot.webapp.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+//@PropertySource("classpath:values.properties") // Carga un archivo de propiedades personalizado
+@PropertySources({
+	@PropertySource("classpath:values.properties") // Se puede cargar varios archivos de propiedades personalizados
+})
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
